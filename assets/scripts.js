d2241ed20884dc39f44c940bcf0acc20b5a0f0ef
Eissa Monet <eissamonet@Eissas-Air.attlocal.net>
2023-06-25 23:34:49 -0500

    changes made to questions

--- assets/scripts.js
+++ assets/scripts.js
@@ -7,6 +7,96 @@ var Questions = [{
         "String",
         "Boolean",
         "Response",
+        "None of the Above"
+    ]
+}]
+
+var Questions = [{
+    number: 2,
+    question: "Which of the following is used to define a variable in JavaScripts?",
+    answer: "var"
+    options: [
+        "getElementById",
+        "var",
+        "getElementBy Class",
+        "None of the Above"
+
+    ]
+}]
+
+var Questions = [{
+    number: 3,
+    question: "Which of the following is NOT a Primitive/Value Type?",
+    answer: "Response"
+    options: [
+        "String",
+        "Boolean",
+        "Response",
+
+
+    ]
+}]
+
+var Questions = [{
+    number: 4,
+    question: "Which of the following is NOT a Primitive/Value Type?",
+    answer: "Response"
+    options: [
+        "String",
+        "Boolean",
+        "Response",
+
+
+    ]
+}]
+
+var Questions = [{
+    number: 5,
+    question: "Which of the following is NOT a Primitive/Value Type?",
+    answer: "Response"
+    options: [
+        "String",
+        "Boolean",
+        "Response",
+
+
+    ]
+}]
+
+var Questions = [{
+    number: 6,
+    question: "Which of the following is NOT a Primitive/Value Type?",
+    answer: "Response"
+    options: [
+        "String",
+        "Boolean",
+        "Response",
+
+
+    ]
+}]
+
+var Questions = [{
+    number: 7,
+    question: "Which of the following is NOT a Primitive/Value Type?",
+    answer: "Response"
+    options: [
+        "String",
+        "Boolean",
+        "Response",
+
+
+    ]
+}]
+
+var Questions = [{
+    number: ,
+    question: "Which of the following is NOT a Primitive/Value Type?",
+    answer: "Response"
+    options: [
+        "String",
+        "Boolean",
+        "Response",
 
 
     ]
