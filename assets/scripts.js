2652c1b3cc69d3d8bcca93834d3923b3b1c8015d
Eissa Monet <eissamonet@Eissas-Air.attlocal.net>
2023-06-26 18:00:16 -0500

    made changes to js

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,37 +2,37 @@
 var questions = [
     {
         questionText: "Which of the following is NOT a Primitive/Value Type?",
-        options: ["1. Strings", "2. Boolean", "3. Response", "4.None of the Above"]
+        options: ["1. Strings", "2. Boolean", "3. Response", "4.None of the Above"],
         answer: "3. Response",
     },
 
     {
         questionText: "Which of the following is used to define a variable in JavaScripts?",
-        options: ["1. getElementById","2. var","3. getElementByClassName","4. None of the Above"]
+        options: ["1. getElementById","2. var","3. getElementByClassName","4. None of the Above"],
         answer: "2. var",
     },
 
     {
         questionText: "An If/Else statement is enclosed within?",
-        options: ["1. Quotation Marks", "2. Parenthesis", "3. Curly Brackets", "4. None of the Above"]
+        options: ["1. Quotation Marks", "2. Parenthesis", "3. Curly Brackets", "4. None of the Above"],
         answer: "2. parenthesis"
     },
 
     {
         questionText: "Arrays in JavaScripts can be used to store?",
-        options: ["1. Strings", "2. Other Arrays", "3. Booleans", "4. All of the Above"]
+        options: ["1. Strings", "2. Other Arrays", "3. Booleans", "4. All of the Above"],
         answer: "4.All of the Above"
     },
 
     {
         questionText: "Which of the following methods is used to access HTML elements using JavaScript?",
-        options: ["1. getElementById", "2. getElementBy ClassName", "3. Both A and B", "4. None of the Above"]
+        options: ["1. getElementById", "2. getElementBy ClassName", "3. Both A and B", "4. None of the Above"],
         answer: "3. Both A and B"
     },
 
     {
         questionText: "JavaScript is an ____ language?",
-        options: ["1. Object-Oriented", "2.Array", "3. Object-Based", "4.None of the Above"]
+        options: ["1. Object-Oriented", "2.Array", "3. Object-Based", "4.None of the Above"],
         answer: "1. Object-Oriented"
     }
 ]
