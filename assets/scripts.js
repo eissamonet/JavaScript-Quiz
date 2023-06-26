b7eb0321a0c90a2014cc65d8053095f7f9db03ac
Eissa Monet <eissamonet@Eissas-Air.attlocal.net>
2023-06-25 22:53:00 -0500

    questions added to js

---
+++ assets/scripts.js
@@ -0,0 +1,13 @@
+// Quiz Questions
+var Questions = [{
+    number: 1,
+    question: "Which of the following is NOT a Primitive/Value Type?",
+    answer: "Response"
+    options: [
+        "String",
+        "Boolean",
+        "Response",
+        "Number"
+
+    ]
+}]
